2c01fe64d57b86e4c3a472239166009188a4079d
Maksim0986 <92432563+Maksim0986@users.noreply.github.com>
2024-07-29 13:15:53 +0300

    dns_stress_test.h

--- dns_stress_test.h
+++ dns_stress_test.h
@@ -1,27 +1,26 @@
 #ifndef DNS_STRESS_TEST_H
 #define DNS_STRESS_TEST_H
 
-#include <arpa/inet.h>
+#include <sys/time.h>
 #include <netinet/in.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
 #include "dns_module.h"
 
 typedef struct {
     int sockfd;
     struct sockaddr_in server_addr;
-    dns_packet_list_t* packet_list;
+    dns_packet_list_t *packet_list;
     int qps;
     int duration;
     int threads;
+    size_t pkt_sent;
+    size_t pkt_recv;
+    size_t pkt_nsent;
+    struct timeval run_time;
+    double estimated_qps;
+    int thread_id;
 } thread_data_t;
 
-void start_stress_test(const char* server_ip, int port, int qps, int duration, int threads);
-void* stress_test_thread(void* arg);
+void start_stress_test(const char *server_ip, int port, int qps, int duration, int threads);
+void print_stats(thread_data_t *data, int num_threads);
 
 #endif // DNS_STRESS_TEST_H
-
